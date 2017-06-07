e98efecd86bb78a331ef82744243d06a42c4e46d
William Ray <wor15@ic.ac.uk>
2017-06-07 16:32:44 +0100

    Added API key verification

---
+++ voters/api_key_verification.py
@@ -0,0 +1,19 @@
+def valid_api_key(key):
+    # Do some magic stuff to verify the API key
+    return False
+
+def has_check_votable_permissions(key):
+    # Do some magic stuff to verify the API key
+    return False
+
+def has_get_voters_permissions(key):
+    # Do some magic stuff to verify the API key
+    return False
+
+def has_make_voter_ineligible_permissions(key):
+    # Do some magic stuff to verify the API key
+    return False
+
+def has_get_candidates_permissions(key):
+    # Do some magic stuff to verify the API key
+    return False
--- voters/views.py
+++ voters/views.py
@@ -1,17 +1,36 @@
 import json
 import datetime
 
+from functools import partial
+
 from django.core import serializers
-from django.http import JsonResponse, HttpResponse
+from django.http import JsonResponse, HttpResponse, HttpResponseForbidden
 from django.core.exceptions import ObjectDoesNotExist
 
 from .models import Voter, Candidate, Station
+from .api_key_verification import valid_api_key, has_check_votable_permissions, has_get_voters_permissions, has_make_voter_ineligible_permissions, has_get_candidates_permissions
+
+UNAUTHORIZED_CODE = 401
+
+def verify(verif):
+    def perform(func):
+        def inner(request, **kwargs):
+            # Does the user have an API key? (should also check they key is valid)
+            if 'API_key' in request.COOKIES and valid_api_key(request.COOKIES['API_key']):
+                # Does the user have appropriate permissions?
+                if verif()(request.COOKIES['API_key']):
+                    return func(request, **kwargs)
+                return HttpResponseForbidden()
+            return HttpResponse(status=UNAUTHORIZED_CODE)
+        return inner
+    return perform
 
 
 def index(request):
     return HttpResponse("The Voter API is online.")
 
 
+@verify(lambda: has_check_votable_permissions)
 def check_votable(request, voter_id):
     try:
         voter = Voter.objects.get(pk=voter_id)
@@ -21,7 +40,7 @@ def check_votable(request, voter_id):
         return JsonResponse({'voter_exists': False,
                              'used_vote': None})
 
-
+@verify(lambda: has_get_voters_permissions)
 def get_voters(request, station_id, voter_name, postcode):
     voters = Voter.objects.filter(
         station=station_id, first_name__iexact=voter_name, postcode__iexact=postcode)
@@ -29,7 +48,7 @@ def get_voters(request, station_id, voter_name, postcode):
     return JsonResponse({'success': voters.count() > 0,
                          'voters': voters_json})
 
-
+@verify(lambda: has_make_voter_ineligible_permissions)
 def make_voter_ineligible(request, voter_id):
     try:
         voter = Voter.objects.get(pk=voter_id)
@@ -50,6 +69,7 @@ def set_voter_has_active_pin(request, voter_id):
         return JsonResponse({'success': False})
 
 
+@verify(lambda: has_get_candidates_permissions)
 def get_candidates(request, station_id):
     try:
         constituency = Station.objects.get(pk=station_id).constituency.pk
